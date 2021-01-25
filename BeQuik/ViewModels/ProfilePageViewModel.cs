5c3f4c0a339fb8536fa9b5ad0cd391abc01859e6
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-01-26 00:03:15 +0200

    -profile page

--- BeQuik/ViewModels/ProfilePageViewModel.cs
+++ BeQuik/ViewModels/ProfilePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,13 @@ namespace BeQuik.ViewModels
 {
     public class ProfilePageViewModel : BaseViewModel
     {
+        public IList ListAreaCode { get; }
+
         public ProfilePageViewModel()
         {
+            ListAreaCode = new List<string> { "JO\t+962", "JO\t+962", "JO\t+962", "JO\t+962", "JO\t+962", };
             OpenPage(new Views.ProfilePage());
         }
+
     }
 }
