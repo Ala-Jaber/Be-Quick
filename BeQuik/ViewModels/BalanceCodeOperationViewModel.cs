484f120cd00eb17a965ae94a3b3e8eec29a28d99
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-01-27 23:16:50 +0200

    -finish generated code operation

--- BeQuik/ViewModels/BalanceCodeOperationViewModel.cs
+++ BeQuik/ViewModels/BalanceCodeOperationViewModel.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace BeQuik.ViewModels
 {
     public class BalanceCodeOperationViewModel : BaseViewModel
     {
+        private bool _IsCodeGenerated;
+        public bool IsCodeGenerated
+        {
+            get { return _IsCodeGenerated; }
+            set { _IsCodeGenerated = value; OnPropertyChanged(); }
+        }
+        public Command GenerateCodeCommand { get; set; }
         public BalanceCodeOperationViewModel()
         {
+            GenerateCodeCommand = new Command(CodeGenerator);
             OpenPage(new Views.BalanceCodeOperationPage());
         }
+        private void CodeGenerator()
+        {
+            IsCodeGenerated = true;
+        }
+
     }
 }
