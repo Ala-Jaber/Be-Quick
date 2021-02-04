1f1ab4cae7cc25bed3eb46eab5052ea20ecdda43
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-02-04 21:47:50 +0200

    -fix show and hide password -fix image icon

---
+++ BeQuik/Triggers/ShowPasswordTriggerAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using XF.Material.Forms.UI;
+
+namespace BeQuik.Triggers
+{
+    public class ShowPasswordTriggerAction : TriggerAction<ImageButton>, INotifyPropertyChanged
+    {
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        private MaterialTextFieldInputType _hidePassword = MaterialTextFieldInputType.Password;
+
+        public MaterialTextFieldInputType HidePassword
+        {
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+            get => _hidePassword;
+        }
+
+        protected override void Invoke(ImageButton sender)
+        {
+            var IsPasswordHiddin = HidePassword == MaterialTextFieldInputType.Password;
+            sender.Source = IsPasswordHiddin ? ShowIcon : HideIcon;
+            HidePassword = IsPasswordHiddin? MaterialTextFieldInputType.Text : MaterialTextFieldInputType.Password;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
