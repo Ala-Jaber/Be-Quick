3ca480fe184d0720e0e8607fb247436d3ead2342
Ala' Jaber <Alaa@dev.gsoft.sa>
2021-02-05 20:30:37 +0200

    -fix booking history

--- BeQuik/ViewModels/BookingHistoryViewModel.cs
+++ BeQuik/ViewModels/BookingHistoryViewModel.cs
@@ -8,11 +8,17 @@ namespace BeQuik.ViewModels
 {
     public class BookingHistoryViewModel: BaseViewModel
     {
-        private ObservableCollection<Model.BookingOrder> _OrdersHistory;
-        public ObservableCollection<Model.BookingOrder> OrdersHistory
+        private ObservableCollection<Model.BookingOrder> _OrdersWeeklyHistory;
+        public ObservableCollection<Model.BookingOrder> OrdersWeeklyHistory
         {
-            get { return _OrdersHistory; }
-            set { _OrdersHistory = value; OnPropertyChanged(); }
+            get { return _OrdersWeeklyHistory; }
+            set { _OrdersWeeklyHistory = value; OnPropertyChanged(); }
+        }
+        private ObservableCollection<Model.BookingOrder> _OrdersMonthlyHistory;
+        public ObservableCollection<Model.BookingOrder> OrdersMonthlyHistory
+        {
+            get { return _OrdersMonthlyHistory; }
+            set { _OrdersMonthlyHistory = value; OnPropertyChanged(); }
         }
         private string _ActiveTabName;
         public string ActiveTabName
@@ -27,7 +33,7 @@ namespace BeQuik.ViewModels
         public BookingHistoryViewModel()
         {
             ActiveTabName = "Weekly";
-            OrdersHistory = new ObservableCollection<Model.BookingOrder>();
+            OrdersWeeklyHistory = new ObservableCollection<Model.BookingOrder>();
             FullListOrder();
             ChangeActiveTabCommand = new Command(ChangeActiveTab);
             ShowDetailsCommand = new Command((e) => ShowDetailsPopup(e as Model.BookingOrder));
@@ -39,7 +45,6 @@ namespace BeQuik.ViewModels
             var PreviousActiveTabName = ActiveTabName;
             if(PreviousActiveTabName == "Weekly")
             {
-                OrdersHistory?.Clear();
                 ActiveTabName = "Monthly";
             }
             else
@@ -58,13 +63,13 @@ namespace BeQuik.ViewModels
         private void BackButtonPressed()=> Application.Current.MainPage.Navigation.PopAsync();
         private void FullListOrder()
         {
-            OrdersHistory ??= new ObservableCollection<Model.BookingOrder>();
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 258, Price = 1.25, Type = Enums.ServiceType.Delivery });
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "tow_truck", OrderID = 208, Price = 10.75, Type = Enums.ServiceType.Winch });
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 158, Price = 1.00, Type = Enums.ServiceType.Delivery });
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 220, Price = 1.15, Type = Enums.ServiceType.Delivery });
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "gas", OrderID = 262, Price = 20.05, Type = Enums.ServiceType.Gas });
-            OrdersHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "taxi", OrderID = 118, Price = 75.00, Type = Enums.ServiceType.Taxi });
+            OrdersMonthlyHistory ??= new ObservableCollection<Model.BookingOrder>();
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 258, Price = 1.25, Type = Enums.ServiceType.Delivery });
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "tow_truck", OrderID = 208, Price = 10.75, Type = Enums.ServiceType.Winch });
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 158, Price = 1.00, Type = Enums.ServiceType.Delivery });
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "delivery", OrderID = 220, Price = 1.15, Type = Enums.ServiceType.Delivery });
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "gas", OrderID = 262, Price = 20.05, Type = Enums.ServiceType.Gas });
+            OrdersMonthlyHistory.Add(new Model.BookingOrder { Date = DateTime.Now, ImageSource = "taxi", OrderID = 118, Price = 75.00, Type = Enums.ServiceType.Taxi });
         }
     }
 }
